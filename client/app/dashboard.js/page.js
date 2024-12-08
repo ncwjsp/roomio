479bd8b984eedffd6d02698474d8c4b949dc3ffd
angytobio <ffewpepp@gmail.com>
2024-12-08 13:18:40 +0700

    Add register page & edit login page

---
+++ client/app/dashboard.js/page.js
@@ -0,0 +1,10 @@
+const Dashboard = () => {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <h1>Dashboard Page</h1>
+      </div>
+    );
+  };
+  
+  export default Dashboard;
+  
---
+++ client/app/register/page.js
@@ -0,0 +1,5 @@
+import Register from "@/app/ui/register";
+
+export default function Home() {
+  return <Register />;
+}
