46902623d50b0f4d6f0a345213fa87338ffe2ce7
ncwjsp <nc.wjsp@gmail.com>
2024-12-09 18:36:15 +0700

    rename dashboard.js to dashboard

---
+++ client/app/dashboard/page.js
@@ -0,0 +1,11 @@
+import Navbar from "@/app/ui/navbar";
+
+const Dashboard = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      Dashboard
+    </div>
+  );
+};
+
+export default Dashboard;
